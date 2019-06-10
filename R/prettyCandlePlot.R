d2bccfe416ec8e11a0484bc862aefb1c83c8e2b5
dominikduda <gospodorz@gmail.com>
2019-06-10 21:10:47 +0200

    Add 'under_candles_layers' arg, autoformat

--- R/prettyCandlePlot.R
+++ R/prettyCandlePlot.R
@@ -1,13 +1,12 @@
-validate_input <- function(time_series) {
+validate_input <- function(time_series, under_candles_layers) {
   assert_data_frame(time_series)
-  if (
-    !test_subset(c('Time', 'Open', 'High', 'Low', 'Close'),
-                 colnames(time_series))
-  ) {
+  if (!test_subset(c('Time', 'Open', 'High', 'Low', 'Close'),
+                   colnames(time_series))) {
     stop("Time series must contain 'Time', 'Open', 'High', 'Low' and 'Close' columns.")
   }
-  time <- time_series[,c('Time')]
+  time <- time_series[, c('Time')]
   assert_posixct(time)
+  assert_list(under_candles_layers)
 }
 
 timeframe <- function(time_series) {
@@ -18,66 +17,84 @@ timeframe <- function(time_series) {
   return(time_frame)
 }
 
-prettyCandlePlot <- function(time_series, chart_title = '') {
-  library(ggplot2)
-  library(checkmate)
+prettyCandlePlot <-
+  function(time_series,
+           chart_title = '',
+           under_candles_layers = list()) {
+    library(ggplot2)
+    library(checkmate)
 
-  validate_input(time_series)
+    validate_input(time_series, under_candles_layers)
 
-  candle_duration <- timeframe(time_series)
-  chart_data <- time_series
-  chart_data$chg = ifelse(chart_data['Close'] > chart_data['Open'], "up", "dn")
-  chart_data$flat_bar <- chart_data[, "High"] == chart_data[, "Low"]
+    candle_duration <- timeframe(time_series)
+    chart_data <- time_series
+    chart_data$chg = ifelse(chart_data['Close'] > chart_data['Open'], "up", "dn")
+    chart_data$flat_bar <- chart_data[, "High"] == chart_data[, "Low"]
 
-  p <- ggplot(chart_data, aes(x = Time))
-  p <- p + theme_bw()
-  p <-
-    p + theme(
-      panel.background = element_rect(fill = 'black'),
-      panel.border = element_blank(),
-      panel.grid.minor = element_blank(),
-      panel.grid.major = element_line(colour = "#121212"),
-      plot.margin = unit(c(10, 0, 0, 32), "pt"),
-      plot.background = element_rect(fill = "black", colour = 'black'),
-      plot.title = element_text(colour = "white"),
-      axis.line = element_line(colour = "white"),
-      axis.text.y = element_text(colour = 'white'),
-      axis.text.x = element_text(
-        colour = 'white',
-        angle = 45,
-        hjust = 1
+    p <- ggplot(chart_data, aes(x = Time))
+    p <- p + theme_bw()
+    p <-
+      p + theme(
+        panel.background = element_rect(fill = 'black'),
+        panel.border = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_line(colour = "#121212"),
+        plot.margin = unit(c(10, 0, 0, 32), "pt"),
+        plot.background = element_rect(fill = "black", colour = 'black'),
+        plot.title = element_text(colour = "white"),
+        axis.line = element_line(colour = "white"),
+        axis.text.y = element_text(colour = 'white'),
+        axis.text.x = element_text(
+          colour = 'white',
+          angle = 45,
+          hjust = 1
+        ),
+        axis.ticks.x = element_line(colour = 'white'),
+        axis.ticks.y = element_line(colour = 'white'),
+        axis.ticks.length = unit(5, "pt")
+      )
+    p <- p + guides(fill = FALSE, colour = FALSE)
+    p <- p + labs(title = chart_title, colour = 'white')
+    for (layer in under_candles_layers) {
+      p <- p + layer
+    }
+    p <-
+      p + layer(
+        geom = 'linerange',
+        mapping = aes(ymin = Low, ymax = High),
+        params = list(color = 'white'),
+        stat = 'identity',
+        position = 'identity'
+      )
+    p <- p + layer(
+      geom = 'rect',
+      mapping = aes(
+        xmin = Time - candle_duration / 2 * 0.9,
+        xmax = Time + candle_duration / 2 * 0.9,
+        ymin = pmin(Open, Close),
+        ymax = pmax(Open, Close),
+        fill = chg
       ),
-      axis.ticks.x = element_line(colour = 'white'),
-      axis.ticks.y = element_line(colour = 'white'),
-      axis.ticks.length = unit(5, "pt")
+      stat = 'identity',
+      position = 'identity'
     )
-  p <-
-    p + geom_linerange(aes(ymin = Low, ymax = High), color = 'white')
-  p <- p + guides(fill = FALSE, colour = FALSE)
-  p <- p + labs(title = chart_title, colour = 'white')
-  p <- p + geom_rect(aes(
-    xmin = Time - candle_duration / 2 * 0.9,
-    xmax = Time + candle_duration / 2 * 0.9,
-    ymin = pmin(Open, Close),
-    ymax = pmax(Open, Close),
-    fill = chg
-  ))
-  p <-
-    p + scale_fill_manual(values = c("dn" = "#656565", "up" = "#ededee"))
-  p <-
-    p + scale_x_datetime(breaks = scales::pretty_breaks(n = 20), date_labels = "")
-  p <-
-    p + scale_y_continuous(position = 'right',
-                           breaks = scales::pretty_breaks(n = 25))
+    p <-
+      p + scale_fill_manual(values = c("dn" = "#656565", "up" = "#ededee"))
+    p <-
+      p + scale_x_datetime(breaks = scales::pretty_breaks(n = 20),
+                           date_labels = "")
+    p <-
+      p + scale_y_continuous(position = 'right',
+                             breaks = scales::pretty_breaks(n = 25))
 
-  # Handle special case of drawing a flat bar where OHLC = Open:
-  if (any(chart_data$flat_bar))
-    p <- p + geom_segment(data = chart_data[chart_data$flat_bar,],
-                          aes(
-                            x = Time - 1 / 2 * 0.9,
-                            y = Close,
-                            yend = Close,
-                            xend = Time + 1 / 2 * 0.9
-                          ))
-  return(p)
-}
+    # Handle special case of drawing a flat bar where OHLC = Open:
+    if (any(chart_data$flat_bar))
+      p <- p + geom_segment(data = chart_data[chart_data$flat_bar, ],
+                            aes(
+                              x = Time - 1 / 2 * 0.9,
+                              y = Close,
+                              yend = Close,
+                              xend = Time + 1 / 2 * 0.9
+                            ))
+    return(p)
+  }
